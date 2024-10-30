223e74f185a54baf41c60f06447113c02fffaeae
julio <juliofifapro@outlook.com>
2024-10-29 22:24:03 -0300

    alteracoes de dados

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import openpyxl
+from urllib.parse import quote
 
 clientes = openpyxl.load_workbook("clientes.xlsx")
 pagina_clientes = clientes['Planilha1']
@@ -10,14 +11,11 @@ for linha in pagina_clientes.iter_rows(min_row=2):
     # nome e telefone.
     nome = linha[0].value
     telefone = linha[1].value
-    print(nome)
-    print(telefone)
-
+    
 for linha in pagina_produtos.iter_rows(min_row=2):
     # produto, preço e descrição
     produto = linha[0].value
     preco = linha[1].value
     descricao = linha[2].value
-    print(produto)
-    print(preco)
-    print(descricao)
+    mensagem = f'ola {nome} esses são os produtos disponoiveis {produto} preço {preco} e descrição: {descricao}'
+    link_mensagem_whatssap = f'https://web.whatssap.com/send?phone={telefone}&text={quote(mensagem)}'
